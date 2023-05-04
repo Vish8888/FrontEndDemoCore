82687584fe2ce8c64464d76b70fb2d29090606db
Vishal Chimkar <vishal.chimkar28@gmail.com>
2023-05-04 23:33:33 +0530

    Add project files.

---
+++ HTMLDemo/Program.cs
@@ -0,0 +1,29 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+}
+
+
+
+//app.UseDefaultFiles(); //index.htm/ index.html / default.htm/default.html
+FileServerOptions options = new FileServerOptions();
+options.DefaultFilesOptions.DefaultFileNames = new[]{"HomeA.html"};
+app.UseFileServer(options);
+
+app.UseStaticFiles();
+
+//app.UseRouting();
+
+//app.UseAuthorization();
+
+//app.MapRazorPages();
+
+app.Run();
